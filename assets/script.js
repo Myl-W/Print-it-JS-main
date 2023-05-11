e70c1a684ad9ad016963d43220cb5a5e483a8052
Myl-W <mylene.wan@gmail.com>
2023-05-11 14:40:31 +0200

    js et html

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,27 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Sélection de l'élément bouton par son ID
+var bouton = document.getElementById("monBouton");
+
+// Ajout de l'event listener pour le clic de souris
+bouton.addEventListener("click", function() {
+    // Code à exécuter lorsque le bouton est cliqué
+    console.log("Le bouton a été cliqué !");
+});
+
+
+function createCarrousel(position){
+	const banner = document.querySelector("banner")
+
+		const element = slides[position];
+		//console.log(élément);
+		const img =document.createElement(img);
+		img.setAttribute("class", "banner-img");
+		img.setAttribute("src", "/assets/images/slideshow/"+ element.image);
+		img.setAttribute("alt", "banner Print-it");
+		const p = document.createElement(p);
+		p.innerhtml= element.tagLine;
+
+		banner.append(p);
